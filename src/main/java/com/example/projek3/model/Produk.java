565534925feb1cd9f305dc210a1c81b29b8d6fbc
helenBelajar <helencindera@gmail.com>
2023-04-23 21:30:27 +0700

    +modelProduk

--- src/main/java/com/example/projek3/model/Produk.java
+++ src/main/java/com/example/projek3/model/Produk.java
@@ -1,5 +1,54 @@
 package com.example.projek3.model;
 
 public class Produk {
+    private int id;
+    private Integer namaProduk;
+    private Integer harga;
+    
+    public Produk() {
+    }
+
+    public Produk(int id, Integer namaProduk, Integer harga) {
+        this.id = id;
+        this.namaProduk = namaProduk;
+        this.harga = harga;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Integer getNamaProduk() {
+        return namaProduk;
+    }
+
+    public void setNamaProduk(Integer namaProduk) {
+        this.namaProduk = namaProduk;
+    }
+
+    public Integer getHarga() {
+        return harga;
+    }
+
+    public void setHarga(Integer harga) {
+        this.harga = harga;
+    }
+
+    @Override
+    public String toString() {
+        return "Produk [id=" + id + ", namaProduk=" + namaProduk + ", harga=" + harga + "]";
+    }
+
+
+
+    
+
+
+
+    
     
 }
