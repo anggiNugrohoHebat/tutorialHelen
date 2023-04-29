9aebe1b44977738c97ff53b759c75176b1411bfd
helenBelajar <helencindera@gmail.com>
2023-04-30 03:45:27 +0700

    +transaksi

--- src/main/java/com/example/projek3/controller/TransaksiController.java
+++ src/main/java/com/example/projek3/controller/TransaksiController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.projek3.dao.ProdukDao;
 import com.example.projek3.dao.TransaksiDao;
 import com.example.projek3.dao.UserDao;
+import com.example.projek3.model.Produk;
 import com.example.projek3.model.Transaksi;
+import com.example.projek3.model.User;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -30,11 +32,17 @@ public class TransaksiController {
     public String save(@RequestBody Transaksi transaksi) {
         try {
             int rows = transaksiDao.save(transaksi);
-        
+            if (rows >= 1) {
+                User user = userDao.getByUserId(transaksi.getUserId());
+                Produk produk = produkDao.selectById(transaksi.getProdukId());
+                int rowterupdate = userDao.updateUser(user.getSaldo(),user.getId() - produk.getHarga());
 
-            
+                return "transaksi berhasil disimpan : " + rows;
+            }
+            return "transaksi gagal id harus unik" + rows;
+        }catch (Exception e) {
+            e.printStackTrace();
+            return "gatot" + e.getMessage();
         }
     }
-
-
 }
